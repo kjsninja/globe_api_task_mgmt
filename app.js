f4d11cc365dd3e49aa5018c6c52d7099cbb5d46b
Ken Crucillo <kendrick004@gmail.com>
2025-04-11 05:29:31 +0800

    add sanitize helper

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const sanitize = require('./helper/sanitize');
 
 const app = express();
 
@@ -7,7 +8,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function(req, res, next) {
-  res.locals.userAgent = req.get('User-Agent');
+  
+  res.locals.userAgent = sanitize.trim(req.get('User-Agent'));
   
   next();
 });
