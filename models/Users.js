70a98efea1a1c710aa4c4ed21708b64ab797e22c
Ken Crucillo <kendrick004@gmail.com>
2025-04-11 05:30:24 +0800

    Change sort order when getting sesion

--- models/Users.js
+++ models/Users.js
@@ -56,6 +56,9 @@ class Users{
 
   async getAllUserSessions(owner){
     return await prisma.userSession.findMany({
+      orderBy: {
+        createdAt: 'desc'
+      },
       where: {
         owner
       }
